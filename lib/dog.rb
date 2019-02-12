1c0a5db02c5e89c5d3be75c50eacccca7fe5a586
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-12 16:13:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,13 @@ class Dog
    dog
  end
 
+ def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      breed = row[2]
+      self.new(name: name, breed: breed, id: id)
+   end
+
  def self.find_by_id(id)
    sql = <<-SQL
       SELECT *
