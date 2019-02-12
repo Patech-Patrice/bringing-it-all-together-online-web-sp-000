2a34080f2c8af22a5f5da4bd1a01b2dad473c3a2
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-12 16:09:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,8 +41,8 @@ class Dog
     self
   end
 
-  def self.create(name, breed)
-   dog = self.new(name, breed)
+  def self.create(name:, breed:)
+   dog = self.new(name; name, breed: breed)
    dog.save
    dog
  end
