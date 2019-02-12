7ba14ebb8ad56da5900aed12dbd60a6991899e29
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-12 16:12:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,5 +47,17 @@ class Dog
    dog
  end
 
+ def self.find_by_id
+   sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+  SQL
+  DB[:conn].execute(sql, id).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+
 
 end
