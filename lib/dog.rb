a42792cb7b849f81674605ad11b5231d9e504719
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-12 16:00:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
   attr_accessor :name, :breed, :id
   #attr_reader :id
 
-  def initialize(id:=nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
     @name = name
     @breed = breed
     @id = id
